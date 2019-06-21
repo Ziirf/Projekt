9aeb1ff311ba504935c283523186809397dc6018
Nicolai Friis <nfriis24@gmail.com>
2019-06-21 19:32:52 +0200

    Customer/Car Overview added

--- Projekt/Frame.cs
+++ Projekt/Frame.cs
@@ -8,7 +8,7 @@ namespace Projekt
 {
     class Frame
     {
-        List<int> DividerVertical = new List<int>();
+        List<int[]> DividerVertical = new List<int[]>();
         List<int[]> DividerHorizontal = new List<int[]>();
 
         private int height;
@@ -39,6 +39,14 @@ namespace Projekt
             set { offsetLeft = value; }
         }
 
+        public Frame(int[] frameDim)
+        {
+            Width = frameDim[0];
+            Height = frameDim[1];
+            OffsetLeft = frameDim[2];
+            OffsetTop = frameDim[3];
+        }
+
         public Frame(int width, int height)
         {
             Width = width;
@@ -47,7 +55,7 @@ namespace Projekt
             OffsetTop = 0;
         }
 
-        public Frame(int width,int height, int offsetLeft, int offsetTop)
+        public Frame(int width, int height, int offsetLeft, int offsetTop)
         {
             Width = width;
             Height = height;
@@ -83,9 +91,9 @@ namespace Projekt
             {
                 for (int i = 0; i < DividerVertical.Count; i++)
                 {
-                    Console.SetCursorPosition(offsetLeft, OffsetTop + DividerVertical[i]);
+                    Console.SetCursorPosition(OffsetLeft + DividerVertical[i][1], offsetTop + DividerVertical[i][0]);
                     Console.Write("╠");
-                    for (int j = 0; j < width - 2; j++)
+                    for (int j = 0; j < DividerVertical[i][2] - DividerVertical[i][1] - 2; j++)
                     {
                         Console.Write("═");
                     }
@@ -101,7 +109,7 @@ namespace Projekt
                     Console.Write("╦");
                     for (int j = 0; j < DividerHorizontal[i][2] - DividerHorizontal[i][1] - 1; j++)
                     {
-                    Console.SetCursorPosition(DividerHorizontal[i][0] + offsetLeft, DividerHorizontal[i][1] + offsetTop + j + 1);
+                        Console.SetCursorPosition(DividerHorizontal[i][0] + offsetLeft, DividerHorizontal[i][1] + offsetTop + j + 1);
                         Console.Write("║");
                     }
                     Console.SetCursorPosition(DividerHorizontal[i][0] + offsetLeft, DividerHorizontal[i][2] + offsetTop);
@@ -110,15 +118,103 @@ namespace Projekt
             }
         }
 
-        public void AddVerticalDivider(int row)
+        public void AddVerticalDivider(int row, int start, int stop)
         {
-            DividerVertical.Add(row);
+            int[] output = { row, start, stop };
+            DividerVertical.Add(output);
         }
 
-        public void AddHorizontalDivider(int collumn,int start, int stop)
+        public void AddHorizontalDivider(int collumn, int start, int stop)
         {
             int[] output = { collumn, start, stop };
             DividerHorizontal.Add(output);
+        }
+
+        // Need further work
+        public void CustomerOverview(int offsetLeft, int offsetTop, int[] buffer)
+        {
+            offsetTop += this.OffsetTop;
+            offsetLeft += this.OffsetLeft;
+
+            Console.SetCursorPosition(offsetLeft + buffer[0], offsetTop);
+            Console.Write("#ID");
+            Console.SetCursorPosition(offsetLeft + buffer[1], offsetTop);
+            Console.Write("FirstName");
+            Console.SetCursorPosition(offsetLeft + buffer[2], offsetTop);
+            Console.Write("LastName");
+            Console.SetCursorPosition(offsetLeft + buffer[3], offsetTop);
+            Console.WriteLine("Zip Code");
+            Console.SetCursorPosition(offsetLeft + buffer[4], offsetTop);
+            Console.WriteLine("City");
+            Console.SetCursorPosition(offsetLeft + buffer[5], offsetTop);
+            Console.Write("Phone Number");
+            Console.SetCursorPosition(offsetLeft + buffer[6], offsetTop);
+            Console.Write("Email");
+            Console.SetCursorPosition(offsetLeft + buffer[7], offsetTop);
+            Console.Write("Customer Since");
+            
+            for (int i = 0; i < Customer.customerList.Count; i++)
+            {
+                Console.SetCursorPosition(offsetLeft + buffer[0], offsetTop + 2 + i);
+                Console.WriteLine(Customer.customerList[i].CustomerID);
+                Console.SetCursorPosition(offsetLeft + buffer[1], offsetTop + 2 + i);
+                Console.WriteLine(Customer.customerList[i].Firstname);
+                Console.SetCursorPosition(offsetLeft + buffer[2], offsetTop + 2 + i);
+                Console.WriteLine(Customer.customerList[i].Lastname);
+                Console.SetCursorPosition(offsetLeft + buffer[3], offsetTop + 2 + i);
+                Console.WriteLine(Customer.customerList[i].ZipCode);
+                Console.SetCursorPosition(offsetLeft + buffer[4], offsetTop + 2 + i);
+                Console.WriteLine(Customer.customerList[i].City);
+                Console.SetCursorPosition(offsetLeft + buffer[5], offsetTop + 2 + i);
+                Console.WriteLine(Customer.customerList[i].PhoneNumber);
+                Console.SetCursorPosition(offsetLeft + buffer[6], offsetTop + 2 + i);
+                Console.WriteLine(Customer.customerList[i].EMail);
+                Console.SetCursorPosition(offsetLeft + buffer[7], offsetTop + 2 + i);
+                Console.WriteLine(Customer.customerList[i].CreatedDate.ToString("dd-MM-yyyy"));
+            }
+        }
+
+        public void CarOverview(int offsetLeft, int offsetTop, int[] buffer)
+        {
+            offsetTop += this.OffsetTop;
+            offsetLeft += this.OffsetLeft;
+
+            Console.SetCursorPosition(offsetLeft + buffer[0], offsetTop);
+            Console.Write("CusID");
+            Console.SetCursorPosition(offsetLeft + buffer[1], offsetTop);
+            Console.Write("VinNumber");
+            Console.SetCursorPosition(offsetLeft + buffer[2], offsetTop);
+            Console.Write("Number Plate");
+            Console.SetCursorPosition(offsetLeft + buffer[3], offsetTop);
+            Console.WriteLine("Car Brand");
+            Console.SetCursorPosition(offsetLeft + buffer[4], offsetTop);
+            Console.WriteLine("Car Model");
+            Console.SetCursorPosition(offsetLeft + buffer[5], offsetTop);
+            Console.Write("Production Year");
+            Console.SetCursorPosition(offsetLeft + buffer[6], offsetTop);
+            Console.Write("Km Count");
+            Console.SetCursorPosition(offsetLeft + buffer[7], offsetTop);
+            Console.Write("Fuel Type");
+
+            for (int i = 0; i < Car.carList.Count; i++)
+            {
+                Console.SetCursorPosition(offsetLeft + buffer[0], offsetTop + 2 + i);
+                Console.WriteLine(Car.carList[i].CustomerID);
+                Console.SetCursorPosition(offsetLeft + buffer[1], offsetTop + 2 + i);
+                Console.WriteLine(Car.carList[i].VinNumber);
+                Console.SetCursorPosition(offsetLeft + buffer[2], offsetTop + 2 + i);
+                Console.WriteLine(Car.carList[i].NumberPlate);
+                Console.SetCursorPosition(offsetLeft + buffer[3], offsetTop + 2 + i);
+                Console.WriteLine(Car.carList[i].CarBrand);
+                Console.SetCursorPosition(offsetLeft + buffer[4], offsetTop + 2 + i);
+                Console.WriteLine(Car.carList[i].CarModel);
+                Console.SetCursorPosition(offsetLeft + buffer[5], offsetTop + 2 + i);
+                Console.WriteLine(Car.carList[i].ProductionYear);
+                Console.SetCursorPosition(offsetLeft + buffer[6], offsetTop + 2 + i);
+                Console.WriteLine(Car.carList[i].KmCount);
+                Console.SetCursorPosition(offsetLeft + buffer[7], offsetTop + 2 + i);
+                Console.WriteLine(Car.carList[i].FuelType);
+            }
         }
     }
 }
--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -11,14 +11,34 @@ namespace Projekt
 
         static void Main(string[] args)
         {
-            Console.SetWindowSize(150, 40);
-            Console.SetBufferSize(150, 40);
+            int[] winSize = { 190, 40 };
+            int[] frameDim = { 190, 40, 0, 1 };
 
-            Frame FrameMain = new Frame(148, 39, 2, 1);
-            FrameMain.AddVerticalDivider(2);
-            FrameMain.AddVerticalDivider(34);
-            FrameMain.AddHorizontalDivider(32, 2, 34);
+            SQL.ReadCustomerToObj();
+            SQL.ReadCarToObj();
+
+            //Console.SetWindowSize(190, 40);
+            //Console.SetBufferSize(190, 40);
+            //Console.WindowLeft = 0;
+
+            //Frame FrameMain = new Frame(190, 39, 0, 1);
+            //FrameMain.AddVerticalDivider(2, 0, 190);
+            //FrameMain.AddVerticalDivider(35, 0, 190);
+            //FrameMain.AddHorizontalDivider(32, 2, 35);
+            //FrameMain.Print();
+            //FrameMain.CustomerOverview(33, 3, new[] { 1, 10, 30, 50, 70, 90, 110 });
+
+            Console.SetWindowSize(winSize[0], winSize[1]);
+            Console.SetBufferSize(winSize[0], winSize[1]);
+            Console.WindowLeft = 0;
+
+            Frame FrameMain = new Frame(frameDim);
+            FrameMain.AddVerticalDivider(1, 0, frameDim[0]);
+            FrameMain.AddVerticalDivider(35, 0, frameDim[0]);
+            FrameMain.AddHorizontalDivider(32, 1, 35);
             FrameMain.Print();
+            //FrameMain.CustomerOverview(33, 3, new[] { 1, 10, 30, 50, 70, 90 ,110, 130 });
+            //FrameMain.CarOverview(33, 3, new[] { 1, 10, 30, 50, 70, 90, 110, 130 });
 
 
             Console.ReadKey();
