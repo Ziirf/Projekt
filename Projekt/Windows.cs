48daa3cabc2ab7ed5900da782abf2054cbe38d99
Nicolai Friis <nico350b@elev.tec.dk>
2019-06-24 10:01:25 +0200

    Window Class added

---
+++ Projekt/Windows.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Windows
+    {
+        public static void MainWindow()
+        {
+
+        }
+
+        public static void CustomerWindow()
+        {
+            Frame frameCustomer = new Frame(Program.frameDim);
+            frameCustomer.AddVerticalDivider(2, 0, Program.frameDim[0]);
+            frameCustomer.AddVerticalDivider(35, 0, Program.frameDim[0]);
+            frameCustomer.AddHorizontalDivider(32, 2, 35);
+
+
+        }
+    }
+}
